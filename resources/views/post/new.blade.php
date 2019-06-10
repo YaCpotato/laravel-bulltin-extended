6e4b468362c3056bdf96062c4aa0b72fba71ea65
YaCpotato <yasshisshy@gmail.com>
2019-06-10 23:49:20 +0900

    dekita

--- resources/views/post/new.blade.php
+++ resources/views/post/new.blade.php
@@ -3,7 +3,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <div class="container">
-    <form action="/post" method="POST">
+    <form action="{{ url('post') }}" method="POST">
     {{ csrf_field() }}
         @method('POST')
         <div class="form-group">
