de990fa7b8ccd2045b24a91f58ee56cc79ddaef5
YaCpotato <yasshisshy@gmail.com>
2019-06-23 17:18:35 +0900

    create user-auth reply and show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,14 +5,14 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <div class="container">
-@if($type != "reply")
+@if($type != "reply" && ($post->UserId == Auth::user()->id))
     {{-- 編集・削除ボタン --}}
     <div class="edit">
         <a href="{{ url('post/'.$post->id.'/edit') }}" class="btn btn-primary">
         編集
         </a>
     </div>
-@else
+@endif
     {{-- 記事内容 --}}
     <dl class="row">
         <dd class="col-md-10">
@@ -25,7 +25,6 @@
     <div id="post-content">
         {{ $post->content }}
     </div>
-@endif
     @if($type == "reply")
         <span>{{ $post->content }}へのリアクトです</span>
         <div class="container">
